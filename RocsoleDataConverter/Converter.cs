a95a572ad4a1b8e79b765a88037ae4aaa439b950
rwajman <rwajman@iis.p.lodz.pl>
2020-09-13 21:07:28 +0200

    UDP receiver and documentation

--- RocsoleDataConverter/Converter.cs
+++ RocsoleDataConverter/Converter.cs
@@ -11,9 +11,14 @@ using System.Threading;
 
 namespace RocsoleDataConverter
 {
-    //How to use DLL in LabView see:
-    //https://www.youtube.com/watch?v=hE0TVHqZjBI
-
+    /// <summary>
+    /// This class is the main of the DLL.
+    /// It consists of the methodes and fields used in LabView.
+    /// </summary>
+    /// <remarks>
+    /// How to use DLL in LabView see:
+    /// https://www.youtube.com/watch?v=hE0TVHqZjBI
+    /// </remarks>
     public class Converter
     {
         [DllImport("kernel32", SetLastError = true)]
@@ -22,6 +27,8 @@ namespace RocsoleDataConverter
 
         private String _TomoKISStudioIP;
         private int _TomoKISStudioPort;
+        private String _UDPIP = "127.0.0.1";
+        private int _UDPPort = 777;
         private int _ElectrodesCount;
         private bool _ConsiderNormalizedData = true;
         private RocsoleFrame lastRocsoleFrame;
@@ -39,14 +46,26 @@ namespace RocsoleDataConverter
         static private bool _TCPClientConnected = false;
         static private Thread _receiver = null;
 
+        /// <value>The IP address of TomoKISStudio module.</value>
         public string TomoKISStudioIP { get => _TomoKISStudioIP; set => _TomoKISStudioIP = value; }
+        /// <value>The TCP port number the TomoKISStudio module is listening on.</value>
         public int TomoKISStudioPort { get => _TomoKISStudioPort; set => _TomoKISStudioPort = value; }
+        /// <value>If true the normalized data are considered otherwise the RAW data retreived directly from Rocsole device.</value>
         public bool ConsiderNormalizedData { get => _ConsiderNormalizedData; set => _ConsiderNormalizedData = value; }
+        /// <value>Gets the index of the current measurement frame.</value>
         public int CurrentRocsoleFrameIndex { get => _currentRocsoleFrameIndex; /*set => _currentRocsoleFrameIndex = value;*/ }
+        /// <value>Gets the time stamp of the current measurement frame.</value>
         public string CurrentRocsoleFrameTimeStamp { get => currentRocsoleFrameTimeStamp; /*set => currentRocsoleFrameTimeStamp = value;*/ }
+        /// <value>Sets the factor A of the transformation equation.</value>
         public double FactorA { get => _factorA; set => _factorA = value; }
+        /// <value>Sets the factor B of the transformation equation.</value>
         public double FactorB { get => _factorB; set => _factorB = value; }
+        /// <value>Sets the electrodes count of the sensor.</value>
         public int ElectrodesCount { get => _ElectrodesCount; set => _ElectrodesCount = value; }
+        /// <value>The IP address of LabView module.</value>
+        public string UDPIP { get => _UDPIP; set { _UDPIP = value; InitializeUDPSocket(_UDPIP, _UDPPort); } }
+        /// <value>The UDP port number the LabViewmodule is listening on.</value>
+        public int UDPPort { get => _UDPPort; set { _UDPPort = value; InitializeUDPSocket(_UDPIP, _UDPPort); } }
 
         public Converter() {
             lastRocsoleFrame = new RocsoleFrame();
@@ -59,9 +78,23 @@ namespace RocsoleDataConverter
             _factorB = 16;
             AllocConsole();
             Console.WriteLine("Converter initialized:\n Using equation: y = "+ _factorA.ToString("0.##") + "x+" + _factorB.ToString("0.##"));
-            InitializeUDPSocket("127.0.0.1", 7);
+            InitializeUDPSocket(_UDPIP, _UDPPort);
         }
 
+        /// <summary>
+        /// Connects to the TomoKISStudio module on IP <paramref name="address"/> and port <paramref name="port"/>.
+        /// </summary>
+        /// <returns>
+        /// <b>True</b> if success.
+        /// See the console windows to check reasons
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// Connect2TomoKISStudio("127.0.0.1", 7);
+        /// </code>
+        /// </example>
+        /// <param name="address">TomoKISStudio IP address.</param>
+        /// <param name="port">TCP port number the TomoKISStudio module is listening on.</param>
         public bool Connect2TomoKISStudio(string address, int port)
         {
             if (CloseTomoKISStudioConnection() == false)
@@ -107,6 +140,13 @@ namespace RocsoleDataConverter
             return false;
         }
 
+        /// <summary>
+        /// Disconnects from the TomoKISStudio module and closes the internal receiving thred/>.
+        /// </summary>
+        /// <returns>
+        /// <b>True</b> if success.
+        /// See the console windows to check reasons
+        /// </returns>
         public bool CloseTomoKISStudioConnection()
         {
             try
@@ -161,7 +201,7 @@ namespace RocsoleDataConverter
                 }
             }
         }
-        static private void StopTcpReceiver()
+        private void StopTcpReceiver()
         {
             try
             {
@@ -174,10 +214,51 @@ namespace RocsoleDataConverter
             }
         }
 
-        
 
-        //The opposite electrode data from lastFilteredFrame are averaged to get a single current value x
-        //then returns y = _factorA * x + _factorB
+        /// <summary>
+        /// Starts processing:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>Connection</term>
+        /// <description>Connects the TomoKISStudio module if not connected yet. </description>
+        /// </item>
+        /// <item>
+        /// <term>New measurement frame</term>
+        /// <description>Retreives a new measurement frame. </description>
+        /// </item>
+        /// <item>
+        /// <term>Filtering</term>
+        /// <description>Filters the opposite electrodes. </description>
+        /// </item>
+        /// <item>
+        /// <term>Averaging</term>
+        /// <description>Calculate average of measurements values of the opposite electrodes to get a single current value (X). </description>
+        /// </item>
+        /// <item>
+        /// <term>Calculation</term>
+        /// <description>Makes calculation Y = factorA * X + factorB. </description>
+        /// </item>
+        /// <item>
+        /// <term>UDP sending</term>
+        /// <description>Sends the (Y) value on UDP to LabView module. </description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <returns>
+        /// <b>Y value</b>. 
+        /// If error returns -1.
+        /// <list type="bullet">
+        /// <item>
+        /// <term>Connection</term>
+        /// <description>Not connected to TomoKISStudio or connection fails. </description>
+        /// </item>
+        /// <item>
+        /// <term>Measurement data format</term>
+        /// <description>Wrong measurement data format. </description>
+        /// </item>
+        /// </list>
+        /// See the console windows to check reasons
+        /// </returns>
         public double ProcessNextFrame()
         {
             if (!_TCPClientConnected)
@@ -194,21 +275,34 @@ namespace RocsoleDataConverter
             double y = _factorA * lastAverage + _factorB;
             Console.WriteLine("Y = " + y.ToString("0.##") + " for frame index = " + _currentRocsoleFrameIndex);
 
+            if (!_UDPSocketInitialized)
+                InitializeUDPSocket("127.0.0.1", 777);
 
             SendValue(y);
 
             return y;
         }
 
-        //sends via UDP on IP:port the value
-        //returns 0 if success otherwise it returns 1
-        //default precision: "0.##"
-        public int SendValue(double value, string precision = "0.##")
+        /// <summary>
+        /// Sends calucated double-type <paramref name="value"/> to the LabView module with the <paramref name="precision"/>
+        /// Default precision is 0.##
+        /// </summary>
+        /// <returns>
+        /// <b>True</b> if success otherwise <b>false</b>.
+        /// See the console windows to check reasons
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// SendValue(10.12345, "0.###") will send 10.123
+        /// SendValue(10.12345) will send 10.12
+        /// </code>
+        /// </example>
+        public bool SendValue(double value, string precision = "0.##")
         {
             if (!_UDPSocketInitialized)
             {
-                Console.WriteLine("First call InitializeUDPSocket(string address, int port) method");
-                return 1;
+                Console.WriteLine("First set the UDPIP and UDPPort properties to initialize.");
+                return false;
             }
             // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.socket.sendto?view=netframework-4.8#System_Net_Sockets_Socket_SendTo_System_Byte___System_Net_EndPoint_
             try
@@ -217,7 +311,7 @@ namespace RocsoleDataConverter
                 // https://stackoverflow.com/questions/2637697/sending-udp-packet-in-c-sharp
                 _UDPSocket.SendTo(Encoding.ASCII.GetBytes(message), _UDPEndPoint);
                 Console.WriteLine($"Send to server ({message.Length}b): " + message);
-                return 0;
+                return true;
             }
             catch (ArgumentNullException)
             {
@@ -231,15 +325,17 @@ namespace RocsoleDataConverter
             {
                 Console.WriteLine("The Socket has been closed.");
             }
-            return 1;
+            return false;
         }
 
-        public void InitializeUDPSocket(string address, int port)
+        private void InitializeUDPSocket(string address, int port)
         {
             // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.socket.-ctor?view=netframework-4.8#System_Net_Sockets_Socket__ctor_System_Net_Sockets_AddressFamily_System_Net_Sockets_SocketType_System_Net_Sockets_ProtocolType
             // https://docs.microsoft.com/en-us/dotnet/api/system.net.ipendpoint.-ctor?view=netframework-4.8#System_Net_IPEndPoint__ctor_System_Net_IPAddress_System_Int32_
             try
             {
+                if (_UDPSocketInitialized)
+                    _UDPSocket.Close();
                 _UDPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                 _UDPEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
                 _UDPSocketInitialized = true;
---
+++ UDPReceiver/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPReceiver
+{
+    class Program
+    {
+        private static int _port = 777;
+        private static UdpClient _server = null;
+        private static IPEndPoint _groupEP = null;
+        private static bool _runServer = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP Server started");
+
+            Console.Write("Default lisiner port is 777, would you like to change it? (yes/no): ");
+            string choose = Console.ReadLine();
+
+            if (choose.Equals("yes"))
+            {
+                Console.Write("Entry port: ");
+                GetNewPort();
+            }
+
+            Console.WriteLine($"Server is working on port {_port}");
+
+            InitializeServer();
+
+            while (_runServer)
+            {
+                SetIPEndPoint(IPAddress.Any);
+                Communication();
+            }
+
+            Console.WriteLine("Closing server.");
+            CloseApplication();
+        }
+
+        static private void CloseApplication()
+        {
+            _server.Close();
+            Environment.Exit(0);
+        }
+
+        static private void Communication()
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.udpclient.receive?view=netframework-4.8
+            // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.udpclient.send?view=netframework-4.8#System_Net_Sockets_UdpClient_Send_System_Byte___System_Int32_System_Net_IPEndPoint_
+            try
+            {
+                byte[] bytesReceived = _server.Receive(ref _groupEP);
+                string message = Encoding.ASCII.GetString(bytesReceived);
+
+                Console.WriteLine($"Received from client ({bytesReceived.Length}b): {message}");
+
+                byte[] bytesToSend = Encoding.ASCII.GetBytes(message);
+                _server.Send(bytesToSend, bytesToSend.Length, _groupEP);
+                Console.WriteLine($"Sent to client ({bytesToSend.Length}b): {message}");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Dgram is null.");
+            }
+            catch (ObjectDisposedException)
+            {
+                Console.WriteLine("The underlying Socket or UdpClient has been closed.");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("UdpClient has already established a default remote host.");
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("An error occurred when accessing the socket.");
+            }
+        }
+
+        static private void SetIPEndPoint(IPAddress address)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/system.net.ipendpoint?view=netframework-4.8
+            try
+            {
+                _groupEP = new IPEndPoint(address, _port);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Address is null.");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("port is less than MinPort or port is greater than MaxPort or address is less than 0 or greater than 0x00000000FFFFFFFF.");
+            }
+        }
+
+        static private void InitializeServer()
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.udpclient.send?view=netframework-4.8
+            try
+            {
+                _server = new UdpClient(_port);
+                SetIPEndPoint(IPAddress.Any);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("The port parameter is greater than MaxPort or less than MinPort.");
+                Environment.Exit(1);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("An error occurred when accessing the socket.");
+                Environment.Exit(1);
+            }
+        }
+
+        static private void GetNewPort()
+        {
+            if (!int.TryParse(Console.ReadLine(), out _port))
+            {
+                Console.WriteLine("Wrong port - there are characters or it is not an integer.");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
